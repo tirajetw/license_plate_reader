8aeccaf0b30489a28d3f423e3e9c5c3de8049671
tirajetw <tirajetw@gmail.com>
2020-02-27 22:13:44 +0700

    version for running test.

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ while True:
             y.sort()
             
         cropped = image[y[1]+bias_pixel:y[2]-bias_pixel, x[1]+bias_pixel:x[2]-bias_pixel]
-        # cv2.imshow("cropped", cropped)
+        cv2.imshow("cropped", cropped)
         cv2.imwrite('crop.jpg', cropped)
         x = pytesseract.image_to_string(Image.open('crop.jpg'), config='-l tha --oem 3 --psm 11')
         print('DATA :\n\n', x)
