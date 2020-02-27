dfe25b0ee4ab76569dee4c20deb460628c244899
tirajetw <tirajetw@gmail.com>
2020-02-27 21:53:30 +0700

    first commit.

--- main.py
+++ main.py
@@ -6,30 +6,32 @@ from PIL import Image
 
 bias_pixel = 5
 
+filename = input('please insert file name.: ')
+
 # Reading Image
-img = cv2.imread("./img/car.jpg")
-cv2.namedWindow("Original Image",cv2.WINDOW_NORMAL)
+img = cv2.imread("./img/" + filename)
+# cv2.namedWindow("Original Image",cv2.WINDOW_NORMAL)
 # Creating a Named window to display image
 # cv2.imshow("Original Image",img)
 # Display image
 
 # RGB to Gray scale conversion
 img_gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
-cv2.namedWindow("Gray Converted Image",cv2.WINDOW_NORMAL)
+# cv2.namedWindow("Gray Converted Image",cv2.WINDOW_NORMAL)
 # Creating a Named window to display image
 # cv2.imshow("Gray Converted Image",img_gray)
 # Display Image
 
 # Noise removal with iterative bilateral filter(removes noise while preserving edges)
 noise_removal = cv2.bilateralFilter(img_gray,9,75,75)
-cv2.namedWindow("Noise Removed Image",cv2.WINDOW_NORMAL)
+# cv2.namedWindow("Noise Removed Image",cv2.WINDOW_NORMAL)
 # Creating a Named window to display image
 # cv2.imshow("Noise Removed Image",noise_removal)
 # Display Image
 
 # Histogram equalisation for better results
 equal_histogram = cv2.equalizeHist(noise_removal)
-cv2.namedWindow("After Histogram equalisation",cv2.WINDOW_NORMAL)
+# cv2.namedWindow("After Histogram equalisation",cv2.WINDOW_NORMAL)
 # Creating a Named window to display image
 # cv2.imshow("After Histogram equalisation",equal_histogram)
 # Display Image
@@ -37,21 +39,21 @@ cv2.namedWindow("After Histogram equalisation",cv2.WINDOW_NORMAL)
 # Morphological opening with a rectangular structure element
 kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(5,5))
 morph_image = cv2.morphologyEx(equal_histogram,cv2.MORPH_OPEN,kernel,iterations=15)
-cv2.namedWindow("Morphological opening",cv2.WINDOW_NORMAL)
+# cv2.namedWindow("Morphological opening",cv2.WINDOW_NORMAL)
 # Creating a Named window to display image
 # cv2.imshow("Morphological opening",morph_image)
 # Display Image
 
 # Image subtraction(Subtracting the Morphed image from the histogram equalised Image)
 sub_morp_image = cv2.subtract(equal_histogram,morph_image)
-cv2.namedWindow("Subtraction image", cv2.WINDOW_NORMAL)
+# cv2.namedWindow("Subtraction image", cv2.WINDOW_NORMAL)
 # Creating a Named window to display image
 # cv2.imshow("Subtraction image", sub_morp_image)
 # Display Image
 
 # Thresholding the image
 ret,thresh_image = cv2.threshold(sub_morp_image,0,255,cv2.THRESH_OTSU)
-cv2.namedWindow("Image after Thresholding",cv2.WINDOW_NORMAL)
+# cv2.namedWindow("Image after Thresholding",cv2.WINDOW_NORMAL)
 # Creating a Named window to display image
 # cv2.imshow("Image after Thresholding",thresh_image)
 # Display Image
@@ -68,7 +70,7 @@ canny_image = cv2.convertScaleAbs(canny_image)
 kernel = np.ones((3,3), np.uint8)
 # Creating the kernel for dilation
 dilated_image = cv2.dilate(canny_image,kernel,iterations=1)
-cv2.namedWindow("Dilation", cv2.WINDOW_NORMAL)
+# cv2.namedWindow("Dilation", cv2.WINDOW_NORMAL)
 # Creating a Named window to display image
 # cv2.imshow("Dilation", dilated_image)
 # Displaying Image
@@ -90,7 +92,7 @@ for c in contours:
         break
 final = cv2.drawContours(img, [screenCnt], -1, (0, 255, 0))
 # Drawing the selected contour on the original image
-cv2.namedWindow("Image with Selected Contour",cv2.WINDOW_NORMAL)
+# cv2.namedWindow("Image with Selected Contour",cv2.WINDOW_NORMAL)
 # Creating a Named window to display image
 # cv2.imshow("Image with Selected Contour",final)
 
@@ -98,7 +100,7 @@ cv2.namedWindow("Image with Selected Contour",cv2.WINDOW_NORMAL)
 mask = np.zeros(img_gray.shape,np.uint8)
 new_image = cv2.drawContours(mask,[screenCnt],0,255,-1,)
 new_image = cv2.bitwise_and(img,img,mask=mask)
-cv2.namedWindow("Final_image",cv2.WINDOW_NORMAL)
+# cv2.namedWindow("Final_image",cv2.WINDOW_NORMAL)
 # cv2.imshow("Final_image",new_image)
 
 # Histogram equal for enhancing the number plate for further processing
@@ -108,7 +110,7 @@ y = cv2.equalizeHist(y)
 # Applying histogram equalisation
 final_image = cv2.cvtColor(cv2.merge([y,cr,cb]),cv2.COLOR_YCrCb2RGB)
 # Merging the 3 channels
-cv2.namedWindow("Enhanced Number Plate",cv2.WINDOW_NORMAL)
+# cv2.namedWindow("Enhanced Number Plate",cv2.WINDOW_NORMAL)
 # Creating a Named window to display image
 # cv2.imshow("Enhanced Number Plate",final_image)
 cv2.imwrite('temp.jpg', new_image)
